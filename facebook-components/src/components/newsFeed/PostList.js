d3926983a70f4f817ff0071572d5cf99728ed767
hogyun3709 <hogyun3709@gmail.com>
2019-12-07 03:53:59 +0900

    Redesign Component Naming convetion

--- facebook-components/src/components/newsFeed/PostList.js
+++ facebook-components/src/components/newsFeed/PostList.js
@@ -1,24 +1,10 @@
 import React, { Component, useState } from "react";
 import PostForm from "./PostForm";
 import CommentForm from "./comment/CommentForm";
-import "./PostList.css"
+import "./PostList.css";
 
-/* 카드 컴포넌트를 만들고, */
-/* This component shows series of post item contents (objects) created by user */
-/* post {} < post의 값만 받고오겠다 */
-const Card = ({ post }) => {
-  const [like, setLike] = useState(0)
-
-  // submitComment() => {
-  //   () => setComment("co")
-  // }
-  //
-  // handleUserEnter = e  => {
-  //   e.key = e.key || e.keyCode;
-  //   if (e.key === 13){
-  //     submitComment()
-  //   }
-  // // }
+const MyPost = ({ postProps }) => {
+  const [like, setLike] = useState(0);
   return (
     <div class="ui card fluid">
       <div class="content">
@@ -30,7 +16,7 @@ const Card = ({ post }) => {
       </div>
       <div class="content">
         <div class="description">
-          <div>{post.message}</div>
+          <div>{postProps.message}</div>
         </div>
         <div class="ui horizontal divider" />
         {/* <span class="right floated">
@@ -52,7 +38,7 @@ const Card = ({ post }) => {
     </div>
   );
 };
-const TempCard = ({ tempPost }) => {
+const TotalPost= ({ totalPostProps }) => {
   const [like, setLike] = useState(0);
 
   return (
@@ -66,7 +52,7 @@ const TempCard = ({ tempPost }) => {
       </div>
       <div class="content">
         <div class="description">
-          <div>{tempPost.body}</div>
+          <div>{totalPostProps.body}</div>
         </div>
         <div class="ui horizontal divider" />
         <span class="right floated">
@@ -79,59 +65,54 @@ const TempCard = ({ tempPost }) => {
         {like} likes
       </div>
       <div class="extra content">
-
         <CommentForm />
 
         {/* <div class="ui large transparent left icon input">
           <i class="heart outline icon"></i>
           <input type="text" placeholder="Add Comment..." />
         </div> */}
-
       </div>
     </div>
   );
 };
-/* comment 를 결국 parent 에서 handling */
 class PostList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       postItems: [],
-      tempPosts: [],
+      totalPosts: []
     };
   }
   getPostData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response =>  response.json())
-    .then(data =>
-      this.setState({
-        tempPosts: data
-      })
-    )
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          totalPosts: data
+        })
+      );
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getPostData();
   }
 
   userInput = messageObj => {
     let linkArray = this.state.postItems;
     linkArray.unshift(messageObj);
-    this.setState({ postItems: linkArray})
+    this.setState({ postItems: linkArray });
   };
 
   render() {
-
     return (
       <div className="postList-wrap">
         <PostForm userInputProps={this.userInput} />
 
         {this.state.postItems.map(post => (
-          <Card post={post} />
+          <MyPost postProps={post} />
         ))}
-        {this.state.tempPosts.map(tempPost =>(
-          <TempCard tempPost={tempPost} />
+        {this.state.totalPosts.map(totalPost => (
+          <TotalPost totalPostProps={totalPost} />
         ))}
-
       </div>
     );
   }
