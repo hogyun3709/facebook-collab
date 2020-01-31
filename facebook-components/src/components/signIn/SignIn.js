d8e501dc1aa708115330242d5752219f9b775f0f
eunji1117 <somsomiii@naver.com>
2020-01-31 16:07:49 +0900

    Add UI to SignIn component

--- facebook-components/src/components/signIn/SignIn.js
+++ facebook-components/src/components/signIn/SignIn.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../images/logo.png';
+import Profile from '../../images/profile.png';
+import UserIco from '../header/account/UserIco';
+import UserName from '../header/account/UserName';
 import './css/SignIn.css';
 
 class SignIn extends React.Component {
@@ -9,8 +12,8 @@ class SignIn extends React.Component {
         this.state = {
             signIn : [
                 {
-                    email: '',
-                    password: ''
+                    email: 'eunji@facebook.io',
+                    password: '1234'
                 }
             ],
             signUp: [
@@ -31,7 +34,7 @@ class SignIn extends React.Component {
         return(
             <div className="signin">
                 <header className="signin__header">
-                    <div className="signin__header__inbox header">
+                    <div className="signin__header__inbox header inbox">
                         <h2 className="header__left">
                             <Link 
                                 to="/"
@@ -72,10 +75,29 @@ class SignIn extends React.Component {
                     </div>
                 </header>
                 <main className="signin__main">
-                    <div className="signin__main__inbox">
-                        <h3 className="main__left">
-                            최근 로그인 기록
-                        </h3>
+                    <div className="signin__main__inbox main inbox">
+                        <div className="main__left signup">
+                            <h3 className="signup__title">
+                                최근 로그인 기록
+                                <span className="signup__title__desc">
+                                    사진을 클릭하고 로그인하세요.
+                                </span>
+                            </h3>
+                            <div className="signup__log">
+                                <span className="signup__log__ico">
+                                    <img src={Profile} alt="프로필"/>
+                                    <span className="signup__log__name">
+                                        <UserName />
+                                    </span>
+                                </span>
+                                <span className="signup__log__ico">
+                                    <img src={Profile} alt="프로필"/>
+                                    <span className="signup__log__name">
+                                        Hokyun
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
                         <div className="main__right signup">
                             <h3 className="signup__title">
                                 새 계정 만들기
@@ -84,107 +106,130 @@ class SignIn extends React.Component {
                                 </span>
                             </h3>
                             <form className="signup__form">
-                                <div className="signup__form__account account">
+                                <div className="signup__form__account input">
                                     <label>
                                         <input 
-                                            className="account__first"
+                                            className="input__signup input__signup-s input__signup-space"
                                             type="text" 
                                             placeholder="성(姓)"
+                                            autofocus="1"
                                         />
                                     </label>
                                     <label>
                                         <input 
-                                            className="account__last"
+                                            className="input__signup input__signup-s"
                                             type="text" 
                                             placeholder="이름(성은 제외)"
                                         />
                                     </label>
                                     <label>
                                         <input 
-                                            className="account__id"
+                                            className="input__signup input__signup-m"
                                             type="email" 
                                             placeholder="휴대폰 번호 또는 이메일"
                                         />
                                     </label>
                                     <label>
                                         <input 
-                                            className="account__pw"
+                                            className="input__signup input__signup-m"
                                             type="password" 
                                             placeholder="새 비밀번호"
                                         />
                                     </label>
                                 </div>
-                                <div className="signup__form__birth">
-                                    <p>생일</p>
-                                    <div>
-                                        <button className="btn--desc">
+                                <div className="info select">
+                                    <p className="info__title">생일</p>
+                                    <select className="select__yyyy select-border">
+                                        <option selected disabled>년도</option>
+                                    </select>
+                                    <select className="select__mm select-border">
+                                        <option selected disabled>월</option>
+                                    </select>
+                                    <select className="select__dd select-border">
+                                        <option selected disabled>일</option>
+                                    </select>
+                                    <div className="info__help">
+                                        <button className="info__help__btn">
                                             도움말
                                         </button>
-                                        <div
-                                            className="btn--desc__desc"
-                                        >
+                                        {/* <div className="info__help__box box">  
+                                            <p className="box__desc">
                                             생일을 입력하면 회원님의 연령에 맞는 Facebook 환경을 이용할 수 있습니다. 
                                             프로필의 정보 섹션에서 이 정보의 공개 범위를 변경할 수 있습니다. 
                                             자세한 내용은 데이터 정책에서 확인해주세요.
-                                        </div>
+                                            </p>
+                                            <div className="box__btn">
+                                                <button className="box__btn-normal box__btn-dark">
+                                                    닫기
+                                                </button>
+                                            </div>
+                                        </div> */}
                                     </div>
                                 </div>
-                                <div className="signup__form__sex">
-                                    <p>성별</p>
+                                <div className="signup__form__info info">
+                                    <p className="info__title">성별</p>
                                     <label>
                                         <input 
-                                            className="sex__w"
+                                            name="gender"
+                                            className="info__radio"
                                             type="radio" 
                                         />
                                         여성
                                     </label>
                                     <label>
                                         <input 
-                                            className="sex__m"
+                                            name="gender"
+                                            className="info__radio"
                                             type="radio" 
                                         />
                                         남성
                                     </label>
                                     <label>
                                         <input 
-                                            className="sex__ect"
+                                            name="gender"
+                                            className="info__radio"
                                             type="radio" />
                                         직접지정
                                     </label>
-                                    <div>
-                                        <button className="btn--desc">
+                                    <div className="info__help">
+                                        <button className="info__help__btn">
                                             도움말
                                         </button>
-                                        <div
-                                            className="btn--desc__desc"
-                                        >
-                                            나중에 프로필에서 회원님의 성별을 볼 수 있는 사람을 변경할 수 있습니다. 
-                                        다른 성별을 선택하거나 성별을 밝히고 싶지 않은 경우 직접 지정을 선택하세요.
-                                        </div>
+                                        {/* <div className="info__help__box box">  
+                                            <p className="box__desc">
+                                                나중에 프로필에서 회원님의 성별을 볼 수 있는 사람을 변경할 수 있습니다. 
+                                                다른 성별을 선택하거나 성별을 밝히고 싶지 않은 경우 직접 지정을 선택하세요.
+                                            </p>
+                                            <div className="box__btn">
+                                                <button className="box__btn-normal box__btn-dark">
+                                                    닫기
+                                                </button>
+                                            </div>
+                                        </div> */}
                                     </div>
-                                    <div>
-                                        <select className="sex__ect__form">
-                                            <option>회원님을 어떻게 지칭할건지 선택하세요</option>
+                                    <div className="info__gender input">
+                                        <select className="info__gender__form">
+                                            <option selected disabled>회원님을 어떻게 지칭할건지 선택하세요</option>
                                             <option>여성: "생일을 축하해주세요!"</option>
                                             <option>남성: "생일을 축하해주세요!"</option>
                                             <option>여러 명: "생일을 축하해주세요!"</option>
                                         </select>
-                                        <span className="sex__ect__desc">
+                                        <p className="info__gender__desc">
                                             선택한 항목이 모든 사람에게 공개됩니다.
-                                        </span>
+                                        </p>
                                         <input 
-                                            className="sex__ect__input"
+                                            className="input__signup input__signup-m"
                                             type="text" 
                                             placeholder="성별(선택 사항)" 
                                         />
                                     </div>
                                 </div>
                                 <p className="signup__form__desc">
-                                    가입하기 버튼을 클릭하면 Facebook의 약관, 데이터 정책 및 쿠키 정책
+                                    가입하기 버튼을 클릭하면 Facebook의 <em>약관</em>, <em>데이터 정책</em> 및 <em>쿠키 정책</em>
                                     에 동의하게 됩니다. Facebook으로부터 SMS 알림을 받을 수 있으며 알
                                     림은 언제든지 옵트 아웃할 수 있습니다.
                                 </p>
-                                <button className="signup__form__submit">
+                                <button className="signup__form__btn">
                                     가입하기
                                 </button>
                             </form>
@@ -192,10 +237,7 @@ class SignIn extends React.Component {
                     </div>
                 </main>
                 <footer className="signin__footer">
-                    <div className="signin__footer__inbox footer">
-                        <div className="footer__top">
-                            뭘 적을까
-                        </div>
+                    <div className="signin__footer__inbox footer inbox">
                         <div className="footer__bottom">
                             Facebook © 2020
                         </div>
