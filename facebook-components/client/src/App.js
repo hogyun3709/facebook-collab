8bae7d69309dbce2e20015ab5339a6cc2a44cce0
eunji1117 <somsomiii@naver.com>
2020-02-04 18:00:28 +0900

    conflict : edit

--- facebook-components/client/src/App.js
+++ facebook-components/client/src/App.js
@@ -11,58 +11,49 @@ import Notice from "./components/header/notice/Notice";
 import SideMenuIndex from "./components/sideMenu/SideMenuIndex";
 import NewsFeed from "./components/newsFeed/NewsFeedIndex";
 import WidgetIndex from "./components/widget/WidgetIndex";
-<<<<<<< HEAD:facebook-components/src/App.js
 import Footer from "./components/footer/Footer";
-
-=======
 import ChatBoard from "./components/chat/ChatBoard";
->>>>>>> 1f62def4201b148c9284d1593e4a5dfccffb3144:facebook-components/client/src/App.js
 
 function App() {
   return (
-    <Router className="App">
+    <>
       <div className="App-container">
-        <header className="App-header-wrap">
-          <div className="App-header">
-            <div className="App-header-left">
-              <Route path="/" component={SearchBar} />
+        <Router className="App">
+          <header className="App-header-wrap">
+            <div className="App-header">
+              <div className="App-header-left">
+                <Route path="/" component={SearchBar} />
+              </div>
+              <div className="App-header-right">
+                <Route path="/" component={Account} />
+                <Route path="/" component={Notice} />
+              </div>
             </div>
-            <div className="App-header-right">
-              <Route path="/" component={Account} />
-              <Route path="/" component={Notice} />
-            </div>
-          </div>
-        </header>
-        {/* <Route exact path="/" component={Home} />
-        <Route path="/User" component={User} />
-        <Route path="/Friends" component={Friends} />
-        <Route path="/Create" component={Create} /> */}
-<<<<<<< HEAD:facebook-components/src/App.js
-        <div className="widget-wrap">
-          <WidgetIndex />
-=======
-      </Router>
+          </header>
+          {/* <Route exact path="/" component={Home} />
+          <Route path="/User" component={User} />
+          <Route path="/Friends" component={Friends} />
+          <Route path="/Create" component={Create} /> */}
+        </Router>
+      </div>
+      <div className="widget-wrap">
+        <WidgetIndex />
+      </div>
       <div className="contents-wrap">
-        <div className="newsFeed-wrap">
-          <NewsFeed/>
-        </div>
+        <nav className="gnb-wrap">
+          <SideMenuIndex />
+        </nav>
+        <main className="newsFeed-wrap">
+          <NewsFeed />
+        </main>
         <div>
           <ChatBoard/>
->>>>>>> 1f62def4201b148c9284d1593e4a5dfccffb3144:facebook-components/client/src/App.js
         </div>
-        <div className="contents-wrap">
-          <nav className="gnb-wrap">
-            <SideMenuIndex />
-          </nav>
-          <main className="newsFeed-wrap">
-            <NewsFeed />
-          </main>
-          <footer className="footer-wrap">
-            <Footer />
-          </footer>
-        </div>
-     </div>
-    </Router>
+        <footer className="footer-wrap">
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 }
 export default App;
--- facebook-components/client/src/components/header/account/UserName.js
+++ facebook-components/client/src/components/header/account/UserName.js
@@ -10,17 +10,6 @@ class UserName extends React.Component{
     };
     render(){
         return(
-<<<<<<< HEAD:facebook-components/src/components/Header/Account/Username.js
-            // <Router>
-            //     <Link
-            //     to="/User"
-            //     className="username">
-                    <span>
-                        {this.state.name}
-                    </span>
-            //     </Link>
-            // </Router>
-=======
             <Router>
                 <Link
                 to="/User"
@@ -29,7 +18,6 @@ class UserName extends React.Component{
                     {this.state.name}
                 </Link>
             </Router>
->>>>>>> 1f62def4201b148c9284d1593e4a5dfccffb3144:facebook-components/client/src/components/header/account/UserName.js
         );
     }
 }
